3f3ac324f3a9af269300ff5f9bca17f0ea953b6c
DESKTOP-G085NET\csi23-nrojlla <csi23-nrojlla@colegioaltair.net>
2024-03-13 11:10:07 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace contarNumeroPalabrasEnCadena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca una frase");
+            string parrafo = Console.ReadLine();
+            Console.WriteLine("Numero de palaras (Split)" + parrafo.Split(" ").Count());
+        }
+    }
+}
